02cfa8d6c9d46c93965ff47cb28db84781edff4f
Ethan Glover <hello@ethang.email>
2024-03-17 18:58:45 -0500

    loggin

--- src/app/api/log/route.ts
+++ src/app/api/log/route.ts
@@ -1,15 +1,20 @@
 import { HTTP_STATUS } from '@ethang/toolbelt/constants/http';
 import { parseFetchJson } from '@ethang/toolbelt/fetch/json';
+import { tryCatchAsync } from '@ethang/toolbelt/functional/try-catch';
 import z from 'zod';
+
+import prisma from '../../../util/prisma';
 
 export async function POST(request: Request) {
   const bodyResults = await parseFetchJson(
     request,
-    z.object({
-      level: z.string(),
-      message: z.string(),
-      timestamp: z.string().datetime(),
-    }),
+    z.array(
+      z.object({
+        level: z.string(),
+        message: z.string(),
+        timestamp: z.string().datetime(),
+      }),
+    ),
   );
 
   if (!bodyResults.isSuccess) {
@@ -18,6 +23,22 @@ export async function POST(request: Request) {
       {
         headers: { 'Content-Type': 'application/json' },
         status: HTTP_STATUS.BAD_REQUEST,
+      },
+    );
+  }
+
+  const log = await tryCatchAsync(async () => {
+    return prisma.log.createMany({
+      data: bodyResults.data,
+    });
+  });
+
+  if (!log.isSuccess) {
+    return Response.json(
+      { error: 'failed to create log' },
+      {
+        headers: { 'Content-Type': 'application/json' },
+        status: HTTP_STATUS.INTERNAL_SERVER_ERROR,
       },
     );
   }
