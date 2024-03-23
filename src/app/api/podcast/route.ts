371ee0552915fa59cb4b4fe5196a0af6e1fbfe73
Ethan Glover <hello@ethang.email>
2024-03-23 17:14:39 -0500

    Update Podcast model and parse YouTube channel ID
    The fields in the Podcast model have been updated in the Prisma schema, with the addition of the 'youtubeChannelId' field. If a podcast is being created with a feed URL from YouTube, the 'youtubeChannelId' is parsed and added to the request during podcast creation. Also, additional dependencies have been added to package.json.

--- src/app/api/podcast/route.ts
+++ src/app/api/podcast/route.ts
@@ -1,6 +1,7 @@
 import { parseFetchJson } from '@ethang/toolbelt/fetch/json';
 import { tryCatchAsync } from '@ethang/toolbelt/functional/try-catch';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { parse } from 'node-html-parser';
 
 import { addPodcastSchema } from '../../../schema/add-podcast';
 import {
@@ -19,12 +20,23 @@ export async function POST(request: Request) {
     return badRequestResponse(bodyResults.error);
   }
 
+  const url = new URL(bodyResults.data.feedUrl);
+  let youtubeChannelId: string | undefined;
+  if (url.hostname.includes('youtube.com')) {
+    const response = await fetch(url);
+    const data = await response.text();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    const parsed = parse(data);
+    const meta = parsed.querySelector('meta[itemprop="identifier"]');
+    youtubeChannelId = meta?.getAttribute('content') ?? undefined;
+  }
+
   const user = await tryCatchAsync(async () => {
     return prisma.user.update({
       data: {
         subscriptions: {
           connectOrCreate: {
-            create: bodyResults.data,
+            create: { ...bodyResults.data, youtubeChannelId },
             where: { feedUrl: bodyResults.data.feedUrl },
           },
         },
